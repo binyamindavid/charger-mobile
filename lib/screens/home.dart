f3330a8c46a3846cfb3acd315bdaf879e338db0f
Oster <benjamin@moozen.com>
2023-09-03 12:53:44 +0100

    dsa

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,7 +4,7 @@ import 'package:go_router/go_router.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @overridesure
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
